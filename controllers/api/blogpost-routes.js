02ecb7fbb9df2f5b51be1aa7a2e631e8272094ef
williamrankin <william.rankin01@gmail.com>
2024-02-17 21:00:04 -0700

    updated routes for comments

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -46,4 +46,38 @@ router.post('/:id/comment', withAuth, async (req, res) => {
   }
 });
 
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlogpost = await Blogpost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlogpost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogpostData) {
+      res.status(404).json({ message: 'No blogpost found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogpostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
