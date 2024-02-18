8e47325a8c0d376d6eb3fdb7e0cb64f0b51e9fab
williamrankin <william.rankin01@gmail.com>
2024-02-17 21:00:34 -0700

    added js for blogpost page

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,45 +1,26 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  console.log(id)
+  event.preventDefault();
 
-    const body = document.querySelector('#comment-body').value.trim();
-  
-    if (body) {
-      const response = await fetch(`/api/blogpost/${id}/comment`, {
-        method: 'POST',
-        body: JSON.stringify({ body }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      if (response.ok) {
-        document.location.replace('/blogpost');
-      } else {
-        alert('Failed to post comment');
-      }
+  const id = event.target.getAttribute('data-id');
+
+  const body = document.querySelector("#comment-body").value.trim();
+
+  if (body) {
+    const response = await fetch(`/api/blogpost/${id}/comment`, {
+      method: "POST",
+      body: JSON.stringify({ body }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace(`/blogpost/${id}`);
+    } else {
+      alert("Failed to post comment");
     }
-  };
+  }
+};
 
-//   const delButtonHandler = async (event) => {
-//     if (event.target.hasAttribute('data-id')) {
-//       const id = event.target.getAttribute('data-id');
-  
-//       const response = await fetch(`/api/projects/${id}`, {
-//         method: 'DELETE',
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/profile');
-//       } else {
-//         alert('Failed to delete project');
-//       }
-//     }
-//   };
-  
-  document
-    .querySelector('.new-comment-form')
-    .addEventListener('submit', newFormHandler);
-  
-//   document
-//     .querySelector('.project-list')
-//     .addEventListener('click', delButtonHandler);
+document
+  .querySelector(".new-comment-form")
+  .addEventListener("submit", newFormHandler);
